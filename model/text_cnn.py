99a6f8aeed4982b50bb54a3bf259a26fa0eef28d
masahiro nishiba <nishiba@masahironoMacBook-Pro.local>
2018-02-11 11:15:44 +0900

    add text cnn model

---
+++ model/text_cnn.py
@@ -0,0 +1,137 @@
+# coding: utf-8
+from enum import Enum
+
+import tensorflow as tf
+from datetime import datetime
+import numpy as np
+
+
+class TaskType(Enum):
+    Classification = 'Classification'
+    Embedding = 'Embedding'
+
+
+class TextCNN(object):
+    def __init__(self, sequence_length, num_classes, vocab_size,
+                 embedding_size, filter_sizes, num_filters,
+                 l2_reg_lambda=0.0, task=TaskType.Classification,
+                 dropout_keep_prob_value = 0.5) -> None:
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
+        self.learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        self.current_learning_rate = 1e-3
+        self.previous_gradients = None
+        self.dropout_keep_prob_value = dropout_keep_prob_value
+        with tf.name_scope('general'):
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+
+        # keeping track of l2 regularization loss
+        l2_loss = tf.constant(0.0)
+
+        # word embedding layer
+        with tf.device('/cpu:0'), tf.name_scope('embedding'):
+            self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name='W')
+            self.embedding_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedding_chars_expanded = tf.expand_dims(self.embedding_chars, -1)
+
+        # create a convolution + max-pool layer for each filter size
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope('conv-maxpool-%s' % filter_size):
+                # Convolution layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name='b')
+                conv = tf.nn.conv2d(self.embedding_chars_expanded, W, strides=[1, 1, 1, 1], padding='VALID', name='conv')
+                # Apply non-linearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
+                # Max-pooling over the outputs
+                pooled = tf.nn.max_pool(h,
+                                        ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                                        strides=[1, 1, 1, 1],
+                                        padding='VALID',
+                                        name='pool')
+                pooled_outputs.append(pooled)
+
+        # combine all the pooled features
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # add dropout
+        with tf.name_scope('drop_out'):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        # scores and predictions
+        with tf.name_scope('drop_out'):
+            W = tf.get_variable('W', shape=[num_filters_total, num_classes], initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name='b')
+            l2_loss += tf.nn.l2_loss(W)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name='score')
+            if task == TaskType.Classification:
+                self.predictions = tf.argmax(self.scores, 1, name='predictions')
+
+        # calculate mean cross-entropy loss
+        with tf.name_scope('loss'):
+            if task == TaskType.Classification:
+                losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            else:
+                losses = tf.losses.mean_squared_error(predictions=tf.tanh(self.scores), labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # calculate accuracy
+        with tf.name_scope("accuracy"):
+            if task == TaskType.Classification:
+                correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+                self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, 'float'), name='accuracy')
+
+    def _train_step(self, x_batch, y_batch, session, train_op, gradients):
+        feed_dict = {self.input_x: x_batch,
+                     self.input_y: y_batch,
+                     self.dropout_keep_prob: self.dropout_keep_prob_value,
+                     self.learning_rate: self.current_learning_rate}
+        _, step, loss, accuracy, grads = session.run([train_op, self.global_step, self.loss, self.accuracy, gradients], feed_dict)
+        self._update_learning_rate(grads)
+
+        time_str = datetime.now().isoformat()
+        print("{}: step {}, loss {:g}, acc {:g}, lr {:g}".format(time_str, step, loss, accuracy, self.current_learning_rate))
+
+    def _test_step(self, x_batch, y_batch, session):
+        feed_dict = {self.input_x: x_batch,
+                     self.input_y: y_batch,
+                     self.dropout_keep_prob: 1.0,
+                     self.learning_rate: self.current_learning_rate}
+        step, loss, accuracy = session.run([self.global_step, self.loss, self.accuracy], feed_dict)
+
+        time_str = datetime.now().isoformat()
+        print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+
+    def _update_learning_rate(self, gradients):
+        if self.previous_gradients is not None:
+            s = np.sum([np.sum(np.multiply(x, y)) for x, y in zip(gradients, self.previous_gradients)])
+            # s = tf.reduce_sum([tf.reduce_sum(tf.multiply(x, y)) for x, y in zip(gradients, self.previous_gradients)])
+            s = tf.clip_by_norm(s, -1., 1.)
+            self.current_learning_rate += 0.001 * s
+            self.current_learning_rate = np.clip(self.current_learning_rate, 1e-8, 1e-2)
+        self.previous_gradients = gradients
+
+    def train(self, batches, test_data, session):
+        x_test, y_test = zip(*test_data)
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        grads_and_vars = optimizer.compute_gradients(self.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
+        gradients = [tf.convert_to_tensor(g) for g, v in grads_and_vars]
+
+        # Initialize all variables
+        session.run(tf.global_variables_initializer())
+
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            self._train_step(x_batch, y_batch, session, train_op, gradients)
+            current_step = tf.train.global_step(session, self.global_step)
+            if current_step % 100 == 0:
+                print("\nEvaluation:")
+                self._test_step(x_test, y_test, session)
+                print("")
---
+++ train.py
@@ -0,0 +1,143 @@
+# coding: utf-8
+
+import time
+from itertools import chain
+from typing import List, Tuple, Dict
+
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+from model.text_cnn import TextCNN
+import os
+
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+import re
+
+
+def _read_file(filepath: str) -> pd.DataFrame:
+    with open(filepath, 'rb') as f:
+        return pd.DataFrame({'review': [str(l) for l in f.readlines()]})
+
+
+def clean_str(string: str) -> List[str]:
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower().split(sep=' ')
+
+
+def batch_iter(data: List[Tuple[np.ndarray, int]], batch_size, num_epochs, do_shuffle=True):
+    data_size = len(data)
+    print(data_size)
+    num_batches_per_epoch = int((data_size - 1) / batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if do_shuffle:
+            data = shuffle(data)
+
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            yield data[start_index:start_index + batch_size]
+
+
+def text_to_index(texts: List[str]) -> Tuple[List[np.ndarray], Dict[str, int]]:
+    split_texts = list(map(clean_str, texts))
+    words = set(list(chain(*split_texts)))
+    dictionary = dict(zip(words, range(len(words))))
+    corpus = [np.array(list(map(lambda x: dictionary[x], text))) for text in split_texts]
+    print(len(corpus))
+    return corpus, dictionary
+
+
+def create_data(max_sentence_length: int = 100):
+    print(os.getcwd())
+    positive_dataset = _read_file(os.path.join('data', 'rt-polaritydata', 'rt-polarity.pos'))
+    positive_dataset['label'] = [np.array([0., 1.]) for _ in range(positive_dataset.shape[0])]
+    negative_dataset = _read_file(os.path.join('data', 'rt-polaritydata', 'rt-polarity.neg'))
+    negative_dataset['label'] = [np.array([1., 0.]) for _ in range(negative_dataset.shape[0])]
+
+    data = pd.concat((positive_dataset, negative_dataset), axis=0)
+    data['review'], dictionary = text_to_index(data['review'].values)
+    eos = len(dictionary)
+    dictionary['eos'] = eos
+    data['review'] = data['review'].apply(lambda x: x[:max_sentence_length])
+    data['review'] = data['review'].apply(lambda x: np.pad(x, (0, max_sentence_length - len(x)), 'constant', constant_values=(0, eos)))
+    return data, dictionary
+
+
+def train(batches,
+          test_data,
+          sequence_length,
+          num_classes,
+          vocab_size,
+          embedding_size,
+          filter_sizes,
+          num_filters,
+          l2_reg_lambda):
+
+    with tf.Graph().as_default():
+        session_conf = tf.ConfigProto(
+            allow_soft_placement=True,
+            log_device_placement=False)
+        sess = tf.Session(config=session_conf)
+        with sess.as_default():
+            cnn = TextCNN(
+                sequence_length=sequence_length,
+                num_classes=num_classes,
+                vocab_size=vocab_size,
+                filter_sizes=filter_sizes,
+                num_filters=num_filters,
+                embedding_size=embedding_size,
+                l2_reg_lambda=l2_reg_lambda)
+
+            # Checkpoint directory.
+            timestamp = str(int(time.time()))
+            out_dir = os.path.abspath(os.path.join(os.path.curdir, 'runs', timestamp))
+            checkpoint_dir = os.path.abspath(os.path.join(out_dir, 'checkpoints'))
+            checkpoint_prefix = os.path.join(checkpoint_dir, 'model')
+            if not os.path.exists(checkpoint_dir):
+                os.makedirs(checkpoint_dir)
+            saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
+
+            # Generate batches
+            cnn.train(batches, test_data, sess)
+            path = saver.save(sess, checkpoint_prefix)
+            print("Saved model checkpoint to {}\n".format(path))
+
+
+def main():
+    max_sentence_length = 60
+    batch_size = 100
+    num_epochs = 5
+
+    dataset, dictionary = create_data(max_sentence_length=max_sentence_length)
+    data = list(zip(dataset['review'], dataset['label']))
+    train_data, test_data = train_test_split(data, random_state=123, test_size=0.1)
+    train_iter = batch_iter(train_data, batch_size=batch_size, num_epochs=num_epochs, do_shuffle=True)
+
+    num_classes = 2
+    vocab_size = len(dictionary)
+    embedding_size = 300
+    filter_sizes = [3, 4, 5]
+    num_filters = 100
+    l2_reg_lambda = 10
+
+    train(train_iter, test_data, sequence_length=max_sentence_length, num_classes=num_classes, vocab_size=vocab_size,
+          embedding_size=embedding_size, filter_sizes=filter_sizes, num_filters=num_filters, l2_reg_lambda=l2_reg_lambda)
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    main()
